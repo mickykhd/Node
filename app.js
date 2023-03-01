7da7173d47ef3dbadc05c83776ea8dc0c756ea04
mickykhd <mcikykhd7@gmail.com>
2023-03-01 22:30:05 +0530

    Node

---
+++ app.js
@@ -0,0 +1,11 @@
+const amount = 9;
+
+if (amount < 10) {
+  console.log("small number");
+} else if (amount < 20) {
+  console.log("medium number");
+} else {
+  console.log("large number");
+}
+
+console.log(`hello world`);
