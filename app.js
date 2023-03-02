5a9fbb4dae968e8d3f9a1786c022329c65ae9c83
mickykhd <mickykhd07@gmail.com>
2023-03-02 17:16:32 +0530

    updated

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const amount = 9;
+// const amount = 9;
 
-if (amount < 10) {
-  console.log("small number");
-} else if (amount < 20) {
-  console.log("medium number");
-} else {
-  console.log("large number");
-}
+// if (amount < 10) {
+//   console.log("small number");
+// } else if (amount < 20) {
+//   console.log("medium number");
+// } else {
+//   console.log("large number");
+// }
 
-console.log(`hello world`);
+// console.log(`hello world`);
+
+// console.log(__dirname);
+// console.log(require);
+// const ab = 1;
+// setInterval(() => {
+//   console.log(ab + 1);
+// }, 1000);
+const name = require("./names.js");
+console.log(name);
---
+++ names.js
@@ -0,0 +1,11 @@
+const micky = "micky";
+const micky2 = "micky2";
+const micky3 = "micky3";
+const micky4 = "micky4";
+
+module.exports = {
+  micky,
+  micky2,
+  micky3,
+  micky4,
+};
